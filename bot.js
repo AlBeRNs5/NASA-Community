419250219fd49bd014be560922af919ea8aa6447
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-01-27 22:02:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '^'
-
-
-
+ 
+ 
+ 
 client.on('ready', () => {
-  console.log(`Logged in as Nasa Bot `);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nasa `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 
@@ -818,4 +840,4 @@ lient.on('message', message => {
     });
     
     
-client.login('NTM5MDkyNDM4Mzg0NTA4OTI4.Dy9UfA.TGfZvcrhBuI4BiTMiTvLB0O15d8');
+client.login(process.env.BOT_TOKEN);
